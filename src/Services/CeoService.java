c849e282f1b72f939e99d561b687df0f6b077b8f
NagyBalazsMark <nagybalazsmark@gmail.com>
2024-11-05 08:54:12 +0200

    Created Service package and CeoService layout

---
+++ src/Services/CeoService.java
@@ -0,0 +1,24 @@
+package src.Services;
+
+public class CeoService {
+
+    public void hireActor(){
+
+    }
+
+    public void fireActor(){
+
+    }
+
+    public void adjustSalary(){
+
+    }
+
+    public void createShow(){
+
+    }
+
+    public void changeTicketPrice(){
+
+    }
+}
