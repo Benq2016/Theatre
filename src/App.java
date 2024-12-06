40bf314925d13db3ab4c9433f40dad4d9aea93d5
NagyBalazsMark <nagybalazsmark@gmail.com>
2024-12-06 17:10:54 +0200

    Created the Connection with the database and created the ActorDBRepository, also commented out the main and I will perform some tests with the DB repos

--- src/App.java
+++ src/App.java
@@ -23,103 +23,6 @@ import static UI.UI.choosingBetweenLoginAndSignup;
 public class App {
 
     /**
-     * Runs some test cases for the system. This method is used for initial testing
-     * of the system's features and doesn't affect the final production result.
-     *
-     * @throws IOException if there is an issue with input or output during the tests
-     */
-    public static void tests() throws IOException {
-//        InMemoryRepository<Admin> ceoInMemoryRepository = new InMemoryRepository<>();
-//        InMemoryRepository<Actor> actorInMemoryRepository = new InMemoryRepository<>();
-//        InMemoryRepository<Auditorium> auditoriumInMemoryRepository= new InMemoryRepository<>();
-//        InMemoryRepository<Show> showInMemoryRepository = new InMemoryRepository<>();
-//        InMemoryRepository<Viewer> viewerInMemoryRepository = new InMemoryRepository<>();
-//        InMemoryRepository<Order> orderInMemoryRepository = new InMemoryRepository<>();
-//
-//        TheatreService ts = new TheatreService(ceoInMemoryRepository, actorInMemoryRepository,
-//                auditoriumInMemoryRepository,showInMemoryRepository, viewerInMemoryRepository,
-//                orderInMemoryRepository);
-//
-//        TheatreController tc = new TheatreController(ts);
-
-
-        //creation of ceo//
-//        EMail eMail = new EMail("bence.c3o@gmail.com", "sosemTalalodKi");
-//        tc.createCeoAccount(1, "BOSS", 24, eMail);
-//        //creation of 2 actors//
-//        EMail eMail1 = new EMail("bazsi123", "1230");
-//        EMail eMail2 = new EMail("anna123", "1232");
-//        tc.ceoHireActor(1, "bazsi", 20, eMail1, 2000);
-//        tc.ceoHireActor(2, "anna", 19, eMail2, 1800);
-//        //assigning roles//
-//        Map<Actor, String> roles = new HashMap<>();
-//        roles.putIfAbsent(tc.viewActor(1), "Romeo");
-//        roles.putIfAbsent(tc.viewActor(2), "Julia");
-//        //creating auditorium//
-//        tc.createAuditorium(1, "Great Sh*t Hole", 10, 20);
-//        //creating show//
-//        tc.createShow(1, "Romeo es Julia!", tc.viewAuditorium(1), roles, "2024-11-15");
-//        tc.createShow(2, "Hatyuk tava", tc.viewAuditorium(1), roles, "2024-11-16");
-//        tc.createShow(3, "Romeo es Julia!", tc.viewAuditorium(1), roles, "2024-11-17");
-//
-//        //creating viewer//
-//        EMail eMailV1 = new EMail("bence.molnar@gmail.com", "1230");
-//        tc.createViewerAccount(1, "bence", 20, eMailV1);
-
-//        System.out.println(tc.viewAllActors());
-//        System.out.println(tc.viewMyShows(eMail1));
-//
-//        //print before order//
-//        System.out.println(tc.viewShows());
-//        System.out.println(tc.viewShow("Romeo es Julia!"));
-//
-//        //fire, adjust actors//
-//        tc.ceoFireActor(1);
-//        tc.ceoChangeSalary(2, 2000);
-//        System.out.println(tc.viewAllActors());
-//
-//        //manage Shows/ Auditoriums//
-//        tc.deleteAuditorium(1);
-//        tc.ceoDeleteShow(2);
-//        System.out.println(tc.viewAllAuditoriums());
-//        System.out.println(tc.viewShows());
-
-        //reserving seats//
-//        List<Integer> seats = new ArrayList<>();
-//        seats.add(66); seats.add(67); seats.add(68); seats.add(69); seats.add(1); seats.add(2); seats.add(150); seats.add(151);
-//
-//        //creating order//
-//        tc.createOrder(1, 1, eMailV1, seats);
-
-        //printing lists and audit after reservation//
-//        System.out.println("\n" + tc.viewMyOrders(eMailV1) +  "\n");
-//        System.out.println(tc.viewOrderTickets(1));
-//
-//        //manage Account for Viewer//
-//        System.out.println(tc.viewAccount(eMailV1));
-//        tc.manageViewerAccount("Bence", 21, eMailV1, eMailV1);
-//        System.out.println(tc.viewAccount(eMailV1));
-//        EMail eMailV1_mod = new EMail("bence.molnar@yahoo.com", "1231");
-//        tc.manageViewerAccount("bence", 20, eMailV1, eMailV1_mod);
-//        System.out.println(tc.viewAccount(eMailV1_mod));
-//
-//        //manage Account for Admin//
-//        System.out.println(tc.viewAccount(eMail));
-//        tc.manageCeoAccount("Bence", 21, eMail, eMail);
-//        System.out.println(tc.viewAccount(eMail));
-//        tc.manageCeoAccount("bence", 20, eMail, eMailV1_mod);
-//        System.out.println(tc.viewAccount(eMailV1_mod));
-//
-//
-//        System.out.println(tc.viewAllActors());
-//        System.out.println(tc.viewAllAuditoriums());
-
-
-//        login(tc);
-
-    }
-
-    /**
      * The main method of the application. This is where the repositories, services, and controllers
      * are instantiated, and the application's flow starts by creating initial objects.
      * It also retrieves user input for login or signup and starts the user interface.
@@ -129,70 +32,72 @@ public class App {
      */
     public static void main(String[] args) throws IOException, ParseException {
 
-        String storageType = chooseStorageType();
-
-        Repository<Admin> adminRepository;
-        Repository<Actor> actorRepository;
-        Repository<Auditorium> auditoriumRepository;
-        Repository<Show> showRepository;
-        Repository<Viewer> viewerRepository;
-        Repository<Order> orderRepository;
-
-        switch (storageType) {
-            case "1": {
-                // In-memory instantiation
-                adminRepository = new InMemoryRepository<Admin>();
-                actorRepository = new InMemoryRepository<Actor>();
-                auditoriumRepository = new InMemoryRepository<Auditorium>();
-                showRepository = new InMemoryRepository<Show>();
-                viewerRepository = new InMemoryRepository<Viewer>();
-                orderRepository = new InMemoryRepository<Order>();
-                break;
-            }
-            case "2": {
-                // File-based instantiation
-
-
-                adminRepository = new AdminFileRepository("src/FilesForStorage/AdminFile");
-                actorRepository = new ActorFileRepository("src/FilesForStorage/ActorFile");
-                auditoriumRepository = new AuditoriumFileRepository("src/FilesForStorage/AuditoriumFile");
-                showRepository = new ShowFileRepository("src/FilesForStorage/ShowFile");
-                viewerRepository = new ViewerFileRepository("src/FilesForStorage/ViewerFile");
-                orderRepository = new OrderFileRepository("src/FilesForStorage/OrderFile");
-                break;
-            }
-            default:
-                throw new IllegalArgumentException("Invalid storage type selected.");
-        }
-
-        ActorService actorService = new ActorService(actorRepository);
-        AdminService adminService = new AdminService(adminRepository);
-        ViewerService viewerService = new ViewerService(viewerRepository);
-        ShowService showService = new ShowService(showRepository);
-        AuditoriumService auditoriumService = new AuditoriumService(auditoriumRepository);
-        OrderService orderService = new OrderService(orderRepository);
-
-
-        TheatreService ts = new TheatreService(adminService, actorService, auditoriumService, showService,
-                viewerService, orderService);
-
-        TheatreController tc = new TheatreController(ts);
-        if (storageType.equals("1")){
-            makeInitialObjects(tc);
-        }
-
-        if (storageType.equals("2")) {
-            ((ShowFileRepository) showRepository).setTheatreController(tc);
-            makeInitialObjects(tc);
-            setInitialStaticIDForEveryDomain(tc);
-        }
-
-//        /*It retrieves the email from the user*/
-        EMail emailGotFromLoginSignIn = choosingBetweenLoginAndSignup(tc);
+//        String storageType = chooseStorageType();
 //
-//        /*Start of the program carrying out ui functions which carries out functions from other parts of the app*/
-        UI ui = new UI(tc);
-        ui.RUN(emailGotFromLoginSignIn);
+//        Repository<Admin> adminRepository;
+//        Repository<Actor> actorRepository;
+//        Repository<Auditorium> auditoriumRepository;
+//        Repository<Show> showRepository;
+//        Repository<Viewer> viewerRepository;
+//        Repository<Order> orderRepository;
+//
+//        switch (storageType) {
+//            case "1": {
+//                // In-memory instantiation
+//                adminRepository = new InMemoryRepository<Admin>();
+//                actorRepository = new InMemoryRepository<Actor>();
+//                auditoriumRepository = new InMemoryRepository<Auditorium>();
+//                showRepository = new InMemoryRepository<Show>();
+//                viewerRepository = new InMemoryRepository<Viewer>();
+//                orderRepository = new InMemoryRepository<Order>();
+//                break;
+//            }
+//            case "2": {
+//                // File-based instantiation
+//
+//
+//                adminRepository = new AdminFileRepository("src/FilesForStorage/AdminFile");
+//                actorRepository = new ActorFileRepository("src/FilesForStorage/ActorFile");
+//                auditoriumRepository = new AuditoriumFileRepository("src/FilesForStorage/AuditoriumFile");
+//                showRepository = new ShowFileRepository("src/FilesForStorage/ShowFile");
+//                viewerRepository = new ViewerFileRepository("src/FilesForStorage/ViewerFile");
+//                orderRepository = new OrderFileRepository("src/FilesForStorage/OrderFile");
+//                break;
+//            }
+//            default:
+//                throw new IllegalArgumentException("Invalid storage type selected.");
+//        }
+//
+//        ActorService actorService = new ActorService(actorRepository);
+//        AdminService adminService = new AdminService(adminRepository);
+//        ViewerService viewerService = new ViewerService(viewerRepository);
+//        ShowService showService = new ShowService(showRepository);
+//        AuditoriumService auditoriumService = new AuditoriumService(auditoriumRepository);
+//        OrderService orderService = new OrderService(orderRepository);
+//
+//
+//        TheatreService ts = new TheatreService(adminService, actorService, auditoriumService, showService,
+//                viewerService, orderService);
+//
+//        TheatreController tc = new TheatreController(ts);
+//        if (storageType.equals("1")){
+//            makeInitialObjects(tc);
+//        }
+//
+//        if (storageType.equals("2")) {
+//            ((ShowFileRepository) showRepository).setTheatreController(tc);
+//            makeInitialObjects(tc);
+//            setInitialStaticIDForEveryDomain(tc);
+//        }
+//
+////        /*It retrieves the email from the user*/
+//        EMail emailGotFromLoginSignIn = choosingBetweenLoginAndSignup(tc);
+////
+////        /*Start of the program carrying out ui functions which carries out functions from other parts of the app*/
+//        UI ui = new UI(tc);
+//        ui.RUN(emailGotFromLoginSignIn);
+
+        testDBStuff();
 
 
     }
@@ -350,6 +255,11 @@ public class App {
                 staticID = actor.getID();
         }
         Person.setIdCounter(staticID);
+
+    }
+
+
+    public static void testDBStuff(){
 
     }
 }
---
+++ src/Repository/ActorDBRepository.java
@@ -0,0 +1,123 @@
+package Repository;
+
+import Domain.Actor;
+import Domain.EMail;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActorDBRepository implements Repository<Actor>{
+    private final String url = "jdbc:sqlserver://localhost:1433;databaseName=Theatre;encrypt=true;trustServerCertificate=true";
+    private final String userName = "SystemAdmin";
+    private final String password = "0000";
+
+    @Override
+    public void create(Actor obj) {
+        String sql = "INSERT INTO Actor(ID,name,age,emailAddress,emailPassword,salary) VALUES(?,?,?,?,?,?)";
+        try (Connection conn = DriverManager.getConnection(url, userName, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, obj.getID());
+            ps.setString(2, obj.getName());
+            ps.setInt(3, obj.getAge());
+            ps.setString(4, obj.getEmail().getEmailAddress());
+            ps.setString(5, obj.getEmail().getPassword());
+            ps.setInt(6, obj.getSalary());
+
+            ps.executeUpdate();
+        }catch (SQLException e) {
+            System.out.println("Error adding Actor into the database");
+        }
+    }
+
+    @Override
+    public void delete(Integer objID) {
+        String sql = "DELETE FROM Actor WHERE ID=?";
+        try (Connection conn = DriverManager.getConnection(url, userName, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, objID);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error deleting Actor from the database");
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void update(Actor obj) {
+        String sql = "UPDATE Actor SET name = ?, age = ?, emailAddress = ?, emailPassword = ?, salary = ? WHERE ID = ?";
+        try (Connection conn = DriverManager.getConnection(url, userName, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, obj.getName());
+            ps.setInt(2, obj.getAge());
+            ps.setString(3, obj.getEmail().getEmailAddress());
+            ps.setString(4, obj.getEmail().getPassword());
+            ps.setInt(5, obj.getSalary());
+            ps.setInt(6, obj.getID());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error updating Actor in the database");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Actor getByID(Integer id) {
+        String sql = "SELECT * FROM Actor WHERE ID = ?";
+        try (Connection conn = DriverManager.getConnection(url, userName, password);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                EMail email = new EMail(rs.getString("emailAddress"), rs.getString("emailPassword"));
+                Actor actor = new Actor(
+                        rs.getInt("ID"),
+                        rs.getString("name"),
+                        rs.getInt("age"),
+                        email,
+                        rs.getInt("salary")
+                );
+                return actor;
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving Actor from the database");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Actor> getAll() {
+        String sql = "SELECT * FROM Actor";
+        List<Actor> actors = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection(url, userName, password);
+             Statement stmt = conn.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                EMail email = new EMail(rs.getString("emailAddress"), rs.getString("emailPassword"));
+                Actor actor = new Actor(
+                        rs.getInt("ID"),
+                        rs.getString("name"),
+                        rs.getInt("age"),
+                        email,
+                        rs.getInt("salary")
+                );
+                actors.add(actor);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving all Actors from the database");
+            e.printStackTrace();
+        }
+        return actors;
+    }
+}
+
